09d4c1512c887a062bb744a7d2334c12bcd52bc8
digikid <dev@digikid.ru>
2023-08-11 01:27:29 +0400

    Update to 1.1.2

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,5 +2,6 @@ declare module '*.vue' {
   import type { DefineComponent } from 'vue';
 
   const component: DefineComponent<Record<never, never>, Record<never, never>, any>;
+
   export default component;
 }
